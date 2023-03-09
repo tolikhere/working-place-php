f611e7dcc57768c999747bcbafa7b9a0277a6ce5
Anatoliy Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-03-09 15:56:13 +0300

    Use namespaces with classes

---
+++ src/PaymentGateway/Paddle/Transaction.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PaymentGateway\Paddle;
+
+class Transaction
+{
+    public function __construct()
+    {
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,1 +1,15 @@
 <?php
+
+require_once '../PaymentGateway/Stripe/Transaction.php';
+require_once '../PaymentGateway/Paddle/CustomerProfile.php';
+require_once '../PaymentGateway/Paddle/Transaction.php';
+
+//use PaymentGateway\Paddle as PD;
+use PaymentGateway\Paddle\{Transaction, CustomerProfile};
+use PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+$paddleTransaction = new Transaction();
+$stripeTransaction = new StripeTransaction();
+$paddleCustomerProfile = new CustomerProfile();
+
+var_dump($paddleTransaction, $stripeTransaction, $paddleCustomerProfile);

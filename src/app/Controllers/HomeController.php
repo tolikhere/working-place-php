0299bfc55f5f848c86dd6973f809d8a56d796922
Anatoliy Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-03-16 16:54:24 +0300

    Use INSERT query and biding methods

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -13,27 +13,41 @@ class HomeController
     {
         try {
             $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', [
+                PDO::ATTR_EMULATE_PREPARES => false,
             ]);
             // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-            $email = $_GET['email'];
-            $query = 'SELECT * FROM  users WHERE email = ?';
 
-            echo $query . '<br>';
+            $email = 'tomd@doe.com';
+            $name = 'Tom Doe';
+            $isActive = 1;
+            $createdAt = date('Y-m-d H:m:i', strtotime('07/11/2021 9:00PM'));
+
+            $query = 'INSERT INTO users (email, full_name, is_active, created_at, updated_at)
+                      VALUES (:email, :name, :active, :date1, :date2)';
 
             $stmt = $db->prepare($query);
 
-            $stmt->execute([$email]);
+            $stmt->bindValue(':name', $name);
+            $stmt->bindValue(':email', $email);
+            $stmt->bindParam(':active', $isActive, PDO::PARAM_BOOL);
+            $stmt->bindValue(':date1', $createdAt);
+            $stmt->bindValue(':date2', $createdAt);
 
-            foreach ($stmt->fetchAll() as $user) {
-                echo '<pre>';
-                var_dump($user);
-                echo '<pre>';
-            }
+            $isActive = 0;
+            $name = 'foo bar';
+
+            $stmt->execute();
+
+            $id = (int) $db->lastInsertId();
+
+            $user = $db->query('SELECT * FROM users WHERE id = ' . $id)->fetch();
+
+            echo '<pre>';
+            var_dump($user);
+            echo '</pre>';
         } catch (\PDOException $e) {
-            throw new \PDOException($e->getMessage(), $e->getCode());
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
         }
-
-        var_dump($db);
 
         return View::make('index', ['foo' => 'bar']);
     }

e268620df6f1aa169cb74a216ce300a6b78c293a
Anatoliy Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-03-16 15:48:43 +0300

    Use SELECT query and the prepare method from sql injections

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -5,30 +5,36 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
+use PDO;
 
 class HomeController
 {
     public function index(): View
     {
+        try {
+            $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', [
+            ]);
+            // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+            $email = $_GET['email'];
+            $query = 'SELECT * FROM  users WHERE email = ?';
+
+            echo $query . '<br>';
+
+            $stmt = $db->prepare($query);
+
+            $stmt->execute([$email]);
+
+            foreach ($stmt->fetchAll() as $user) {
+                echo '<pre>';
+                var_dump($user);
+                echo '<pre>';
+            }
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), $e->getCode());
+        }
+
+        var_dump($db);
+
         return View::make('index', ['foo' => 'bar']);
-    }
-
-    public function download()
-    {
-        header('Content-Type: image/png');
-        header('Content-Disposition: attachment;filename="myfile.png"');
-
-        readfile(STORAGE_PATH . '/night.png');
-    }
-
-    public function upload()
-    {
-        $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
-
-        move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
-
-        header('Location: /');
-
-        exit;
     }
 }
--- src/views/index.php
+++
@@ -1,6 +0,0 @@
-<h1><?= $foo ?></h1>
-
-<form action="/upload" method="post" enctype="multipart/form-data">
-    <input type="file" name="receipt">
-    <button type="submit">Upload</button>
-</form>
